459c8fcfe6f81fa336ab2b1d7b4ded0c1934394e
Mihail-sudo <321mig123@gmail.com>
2025-05-25 08:46:25 +0500

    first iteration

---
+++ src/bot.py
@@ -0,0 +1,24 @@
+import os
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+
+
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from handlers import router
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
