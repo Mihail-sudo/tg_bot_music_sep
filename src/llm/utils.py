90a23d74a692fff5d4031560e8317afcb6cf109a
Mihail-sudo <321mig123@gmail.com>
2025-05-28 17:39:37 +0500

    added a function of finding lyrics for song

--- src/llm/utils.py
+++ src/llm/utils.py
@@ -1,4 +1,7 @@
 import tiktoken
+import httpx
+import urllib.parse
+from typing import Optional
 
 
 def token_counter(messages):
@@ -7,3 +10,30 @@ def token_counter(messages):
     for msg in messages:
         total += len(enc.encode(msg.content))
     return total
+
+
+async def search_lyrics(artist: str, title: str, timeout: int = 10) -> Optional[str]:
+    base_url = "https://api.lyrics.ovh/v1/"
+    encoded_artist = urllib.parse.quote(artist)
+    encoded_title = urllib.parse.quote(title)
+    url = f"{base_url}{encoded_artist}/{encoded_title}"
+
+
+    try:
+        async with httpx.AsyncClient(timeout=timeout) as client:
+            response = await client.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            if 'lyrics' in data:
+                lyrics = data['lyrics']
+                return lyrics
+            else:
+                return "Текст песни не найден в ответе API"
+
+    except httpx.HTTPStatusError as e:
+        print(f"Ошибка HTTP: {e}")
+        return None
+    except httpx.RequestError as e:
+        print(f"Ошибка сети: {e}")
+        return None
