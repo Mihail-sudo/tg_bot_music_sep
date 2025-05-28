43bb0722347b90f078ad7ab28aed42c511a49c97
Mihail-sudo <321mig123@gmail.com>
2025-05-28 15:29:39 +0500

    architecture changes

--- src/llm/llm.py
+++ src/llm/llm.py
@@ -1,6 +1,6 @@
 from .llm_abs import LLMService, AnswerDTO, QuestionDTO
-from typing import cast
-from langchain_core.messages import AIMessage, trim_messages
+from typing import Callable, Dict, cast
+from langchain_core.messages import AIMessage, BaseMessage, trim_messages
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.chat_history import InMemoryChatMessageHistory, BaseChatMessageHistory
@@ -16,14 +16,23 @@ class OllamaLLMService(LLMService):
     ]
 
     def __init__(self, model_name: str, ollama_base_url: str, api_key: str):
-        llm = ChatOpenAI(
+        self._model = self._create_model(model_name, ollama_base_url, api_key)
+        self._promt = self._create_promt()
+        self._trimmer = self._create_trimmer()
+        self._chain = self._create_chain()
+
+    def _create_model(self, model_name: str, base_url: str, api_key: str) -> ChatOpenAI:
+        return ChatOpenAI(
             model=model_name,
-            base_url=f"{ollama_base_url}/v1",
+            base_url=f"{base_url}/v1",
             api_key=api_key
         )
-        prompt = ChatPromptTemplate(self._MESSAGES)
-
-        trimmer = trim_messages(
+    
+    def _create_promt(self) -> ChatPromptTemplate:
+        return ChatPromptTemplate(self._MESSAGES)
+    
+    def _create_trimmer(self) -> Callable[[list[BaseMessage]], list[BaseMessage]]:
+        return trim_messages(
             strategy="last",
             token_counter=token_counter,
             max_tokens=512,
@@ -31,15 +40,18 @@ class OllamaLLMService(LLMService):
             include_system=True,
             allow_partial=False
         )
+    
+    def _create_chain(self) -> RunnableWithMessageHistory:
+        store: Dict[str, BaseChatMessageHistory] = {}
 
-        self.store = {}
         def get_session_history(session_id: str) -> BaseChatMessageHistory:
-            if session_id not in self.store:
-                self.store[session_id] = InMemoryChatMessageHistory()
-            return self.store[session_id]
+            if session_id not in store:
+                store[session_id] = InMemoryChatMessageHistory()
+            return store[session_id]
 
-        chain = prompt | trimmer | llm
-        self._chain = RunnableWithMessageHistory(
+        chain = self._promt | self._trimmer | self._model
+
+        return RunnableWithMessageHistory(
             chain,
             get_session_history,
             input_messages_key="question",
